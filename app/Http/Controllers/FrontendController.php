78f29328745cfac40bddc36bf86192087154a901
gilbertozioma <gilbertozioma0@gmail.com>
2024-12-05 23:46:12 +0100

    loaded index view

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -5,20 +5,20 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class FrontendController extends Controller
 {
     public function index() 
     {
         $products = Product::all();
-        return Inertia::render('Frontend/Home', compact('products'));
-
-        // return Inertia::render('Welcome', [
-        //     'canLogin' => Route::has('login'),
-        //     'canRegister' => Route::has('register'),
-        //     'laravelVersion' => Application::VERSION,
-        //     'phpVersion' => PHP_VERSION,
-        // ]);
+        
+        return Inertia::render('Index', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+        ]);
+        
+        // return Inertia::render('Frontend/Home', compact('products'));
     }
 
     public function about() 
